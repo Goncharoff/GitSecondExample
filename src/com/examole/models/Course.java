3abb514ccdad7b71c75c9f2957d84b4a7c23e6c7
Nik <nik.ignition.work@gmail.com>
2022-10-17 20:33:30 +0300

    added course functionality

---
+++ src/com/examole/models/Course.java
@@ -0,0 +1,10 @@
+package com.examole.models;
+
+public class Course {
+    private String myConflictSetup;
+
+    public void setConflict() {
+        Lecture lecture = new Lecture();
+        Lecture lecture1 = new Lecture();
+    }
+}
